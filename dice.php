ccb3d382882cd9794499547ce778456067c9bc91
bouakline <mohamed.bouakline@hotmail.com>
2022-01-14 10:42:59 +0100

    ameloration du jeu de des
    tirage de trois dés avec une boucle
    
    (alice)

--- dice.php
+++ dice.php
@@ -1,3 +1,18 @@
 <?php
+
+$numbers = [];
+$iterations = 3;
+
+for ($i =0;  $i < $iterations;$i++){
+    $numbers[]= random_int (1, 6)
+}
+
 $n =random_int(1, 6);
+ 
+foreach( $numbers as $number){
+    echo "-un $number<br>\n";
+}
+
+
+
 echo "vous tiré un $n<br>\n";
